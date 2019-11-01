e61d75a3ef4c9df1a29e6025ea1fcac3b1ba36f9
marcacohen <marcacohen@gmail.com>
2019-10-31 23:21:13 -0400

    fix time frame logic

--- pageviews.sh
+++ pageviews.sh
@@ -45,14 +45,25 @@ Connection: keep-alive\r\n\r\n
 EOF
 )"
 
-YESTERDAY=$(($(date '+%s') - 86400))
+
+TODAY=$(date '+%s')
+YESTERDAY=$(($TODAY - 86400))
 YYYY=$(date --date=@$YESTERDAY +%Y)
 MM=$(date --date=@$YESTERDAY +%m)
 DD=$(date --date=@$YESTERDAY +%d)
-if   [ "$WINDOW" = "all" ];   then S1=/;                S2=pageviews-*.gz
-elif [ "$WINDOW" = "year" ];  then S1=/$YYYY;           S2=pageviews-*.gz
-elif [ "$WINDOW" = "month" ]; then S1=/$YYYY/$YYYY-$MM; S2=pageviews-*.gz
-elif [ "$WINDOW" = "day" ];   then S1=/$YYYY/$YYYY-$MM; S2=pageviews-$YYYY$MM$DD-*.gz
+
+if [ "$WINDOW" = "all" ]
+then
+  S1=/; S2=*/*/ S3=pageviews-*.gz
+elif [ "$WINDOW" = "year" ]
+then
+  S1=/$YYYY/; S2=*/; S3=pageviews-$YYYY*.gz
+elif [ "$WINDOW" = "month" ]
+then 
+  S1=/$YYYY/$YYYY-$MM/; S2=; S3=pageviews-$YYYY$MM*.gz
+elif [ "$WINDOW" = "day" ]
+then 
+  S1=/$YYYY/$YYYY-$MM/; S2=; S3=pageviews-$YYYY$MM$DD-*.gz
 fi
 
 if [ ! -z ${K_SERVICE+x} ]
@@ -61,17 +72,21 @@ then
   echo -en "TsvHttpData-1.0$EOL"
 fi
 
+echo wget --no-parent -nv --spider -S -r -A "$S3" $SRC_VIEW_URL$S1
+echo gsutil -o 'Boto:https_validate_certificates=False' ls -l -r $DST_VIEW_URL$S1$S2$S3
+exit
+
 # Assemble list of every pageview log file and size on website.
-wget --no-parent -nv --spider -S -r -A "$S2" $SRC_VIEW_URL/$S1/ 2>&1 |
+wget --no-parent -nv --spider -S -r -A "$S3" $SRC_VIEW_URL$S1 2>&1 |
   awk 'function base(file, a, n) {n = split(file,a,"/"); return a[n]} \
        $1 == "Content-Length:" {len=$2} $3 == "URL:" {print base($4), len}' |
   sort >src-files.txt
 
 # Assemble list of every pageview log file and size in cloud storage.
 >dst-files.txt
-if gsutil -q stat $DST_VIEW_URL$S1/$S2 >/dev/null 2>&1
+if gsutil -q stat $DST_VIEW_URL$S1$S2$S3 >/dev/null 2>&1
 then
-  gsutil -o 'Boto:https_validate_certificates=False' ls -l -r $DST_VIEW_URL$S1/$S2 2>/dev/null | grep -v ":$" |
+  gsutil -o 'Boto:https_validate_certificates=False' ls -l -r $DST_VIEW_URL$S1$S2$S3 2>/dev/null | grep -v ":$" |
     awk 'function base(file, a, n) {n = split(file,a,"/"); return a[n]} \
          $1 != "TOTAL:" {print base($3), $1}' | sort >dst-files.txt
 fi
