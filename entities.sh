b374dc468004c54b566ee4181a463a7f2cbc179f
marcacohen <marcacohen@gmail.com>
2019-11-03 17:43:54 +0000

    split entities into sep script

--- entities.sh
+++ entities.sh
@@ -51,46 +51,25 @@ YYYY=$(date --date=@$TODAY +%Y)
 MM=$(date --date=@$TODAY +%m)
 DD=$(date --date=@$TODAY +%d)
 
-if [ "$WINDOW" = "all" ]
-then
-  S1=/; S2=*/*/ S3=pageviews-*.gz
-elif [ "$WINDOW" = "year" ]
-then
-  S1=/$YYYY/; S2=*/; S3=pageviews-$YYYY*.gz
-elif [ "$WINDOW" = "month" ]
-then 
-  S1=/$YYYY/$YYYY-$MM/; S2=; S3=pageviews-$YYYY$MM*.gz
-elif [ "$WINDOW" = "day" ]
-then 
-  S1=/$YYYY/$YYYY-$MM/; S2=; S3=pageviews-$YYYY$MM$DD-*.gz
-fi
-
 if [ ! -z ${K_SERVICE+x} ]
 then
   echo -en "$HEAD" 
 fi
 echo -en "TsvHttpData-1.0$EOL"
 
-# Assemble list of every pageview log file and size on website.
->src-files.txt
-wget --no-parent -nv --spider -S -r -A "$S3" $SRC_VIEW_URL$S1 2>&1 |
-awk 'function base(file, a, n) {n = split(file,a,"/"); return a[n]} \
-     $1 == "Content-Length:" {len=$2} $3 == "URL:" {print base($4), len}' |
-sort >>src-files.txt
-
-# Assemble list of every pageview log file and size in cloud storage.
->dst-files.txt
-if gsutil -o 'Boto:https_validate_certificates=False' stat $DST_VIEW_URL$S1$S2$S3 >/dev/null 2>&1
+# if today is Sunday, arrange to get compressed wikidata file.
+if [[ $(date +%u) -eq 7 ]]
 then
-  gsutil -o 'Boto:https_validate_certificates=False' ls -l -r $DST_VIEW_URL$S1$S2$S3 2>/dev/null | grep -v ":$" |
-  awk 'function base(file, a, n) {n = split(file,a,"/"); return a[n]} \
-       $1 != "TOTAL:" {print base($3), $1}' | sort >>dst-files.txt
+  read SFILE SSIZE \
+    <<<$(wget -nv --spider -S -r -A ".gz" -I $SRC_DATA_PATH $SRC_DATA_URL 2>&1 |
+         awk 'function base(file, a, n) {n = split(file,a,"/"); return a[n]} \
+              $1 == "Content-Length:" {len=$2} $3 == "URL:" {print base($4), len}')
+  read DFILE DSIZE \
+    <<<$(gsutil ls -l -r $DST_DATA_URL |
+         awk 'function base(file, a, n) {n = split(file,a,"/"); return a[n]} \
+              $1 != "TOTAL:" {print base($3), $1}')
+  if [ "$SFILE" != "$DFILE" -o "$SSIZE" != "$DSIZE" ]
+  then
+    echo $SRC_DATA_URL
+  fi
 fi
-
-# One-sided diff - every file that doesn't exist or match size in cloud storage.
-comm -23 src-files.txt dst-files.txt |
-while read FILE SIZE
-do
-  DIR=`echo $FILE | awk '{y=substr($1,11,4);m=substr($1,15,2); printf("%s/%s-%s",y,y,m)}'`
-  echo -en "$SRC_VIEW_URL/$DIR/$FILE$EOL"
-done
