86228bd2e87b6c76ea44b6a9fc71637854998229
marcacohen <marcacohen@gmail.com>
2019-11-04 16:44:45 +0000

    refine vm startup logic

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
-#! /bin/bash
+#!/usr/bin/env bash
+
 sudo apt-get update
 sudo apt-get install -y wget lbzip2
 
@@ -14,6 +15,6 @@ time gsutil -o GSUtil:parallel_composite_upload_threshold=150M cp latest-all.jso
 echo loading...
 time bq load --field_delimiter="tab" --max_bad_records 1 --replace wikidata.latest_raw gs://wiki-staging/latest-all.json item
 
-echo self-destroying...
-gcloud compute instances delete $(hostname) --zone \
-  $(curl -H Metadata-Flavor:Google http://metadata.google.internal/computeMetadata/v1/instance/zone -s | cut -d/ -f4)"
+echo self-destructing...
+#gcloud compute instances delete $(hostname) --zone \
+  #$(curl -H Metadata-Flavor:Google http://metadata.google.internal/computeMetadata/v1/instance/zone -s | cut -d/ -f4)
--- wikiload.sh
+++ wikiload.sh
@@ -16,11 +16,13 @@
 
 set -eEuo pipefail
 
-BUCKET=wiki-staging
-DOMAIN=dumps.wikimedia.org
-DST_BASE=gs://$BUCKET
-DST_DATA_PATH=$DOMAIN/$SRC_DATA_PATH
-DST_DATA_URL=$DST_BASE/$DST_DATA_PATH
+if [ -z ${K_SERVICE+x} ]
+then
+  EOL="\n"
+else
+  EOL="\r\n"
+fi
+
 VMNAME=wikiload
 ZONE=us-central1-c
 SCOPES="https://www.googleapis.com/auth/compute,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/trace.append,https://www.googleapis.com/auth/devstorage.read_only"
@@ -36,12 +38,25 @@ then
   echo -en "$HEAD" 
 fi
 
+# MAKE THIS VM PREEMPTIBLE!
+# and generalize startup script
 echo "creating VM...$EOL"
 gcloud compute instances create $VMNAME \
+
+gcloud beta compute instances create $VMNAME \
   --zone=$ZONE \
-  --image-family=ubuntu-1804-lts \
-  --image-project=ubuntu-os-cloud \
-  --boot-disk-size=200GB \
-  --scopes=$SCOPES \
+  --machine-type=m1-ultramem-80 \
+  --subnet=default \
+  --network-tier=PREMIUM \
+  --no-restart-on-failure \
+  --maintenance-policy=TERMINATE \
+  --preemptible \
+  --scopes=https://www.googleapis.com/auth/cloud-platform \
+  --image=debian-9-drawfork-v20191004 \
+  --image-project=eip-images \
+  --boot-disk-size=1000GB \
+  --boot-disk-type=pd-ssd \
+  --boot-disk-device-name=wikiload \
+  --reservation-affinity=any \
   --metadata-from-file startup-script=startup.sh
 echo "DONE.$EOL"
